ca2ffaeafdcbae270527befb52948d5e4033308b
felipe.pascoal <bd.bumdigital@gmail.com>
2022-04-06 01:13:17 -0300

    Exercicio 071

---
+++ Exercicio069/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercicio069
+{
+    //69) Crie um programa que leia a idade e o sexo
+    //de várias pessoas. A cada pessoa cadastrada, o
+    //programa deverá perguntar se o usuário quer ou
+    //não continuar. No final, mostre:
+    //A) quantas pessoas tem mais de 18 anos.
+    //B) quantos homens foram cadastrados.
+    //C) quantas mulheres tem menos de 20 anos.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pessoasM18 = 0;
+            int homensCdast = 0;
+            int mulheresM20 = 0;
+            string continuar = "";
+            while (continuar != "N")
+            {
+                Console.Write("Digite sua idade: ");
+                int idade = int.Parse(Console.ReadLine());
+                Console.Write("DIgite o sexo [F/M]: ");
+                string sexo = Console.ReadLine().ToUpper(); ; 
+                while (sexo != "F" && sexo != "M")
+                {
+                    Console.Write("Valor invalido para sexo. Digite novamento [F/M]: ");
+                    sexo = Console.ReadLine().ToUpper();
+                }
+                if (idade > 18)
+                {
+                    pessoasM18 += 1;
+                }
+                if (sexo == "M")
+                {
+                    homensCdast += 1;
+                }
+                if (idade < 20 && sexo == "F")
+                {
+                    mulheresM20 += 1;
+                }
+                Console.Write("Quer continuar? [S/N]: ");
+                continuar = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine($"{pessoasM18} pessoas tem mais de 18 anos");
+            Console.WriteLine($"{homensCdast} homens foram cadastrados");
+            Console.WriteLine($"{mulheresM20} mulheres tem menos de 20 anos");
+        }
+    }
+}
---
+++ Exercicio071/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercicio071
+{
+    //71) Crie um programa que simule o funcionamento de
+    //um caixa eletrônico. No início, pergunte ao usuário
+    //qual será o valor a ser sacado (número inteiro) e o
+    //programa vai informar quantas cédulas de cada valor
+    //serão entregues.
+    //OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor que deseja sacar R$");
+            int valor = int.Parse(Console.ReadLine());
+            int cinquenta = 0;
+            int vinte = 0;
+            int dez = 0;
+            int um = 0;
+            while (valor > 0)
+            {
+                if (valor - 50 >= 0)
+                {
+                    valor -= 50;
+                    cinquenta += 1;
+                } else if (valor - 20 >= 0)
+                {
+                    valor -= 20;
+                    vinte += 1;
+                } else if (valor - 10 >= 0)
+                {
+                    valor -= 10;
+                    dez += 1;
+                } else if (valor - 1 >= 0)
+                {
+                    valor -= 1;
+                    um += 1;
+                }
+            }
+            if (cinquenta > 0)
+            {
+                Console.WriteLine($"{cinquenta} notas de 50");
+            }
+            if (vinte > 0)
+            {
+                Console.WriteLine($"{vinte} notas de 20");
+            }
+            if (dez > 0)
+            {
+                Console.WriteLine($"{dez} notas de 10");
+            }
+            if (um > 0)
+            {
+                Console.WriteLine($"{um} notas de 1");
+            }      
+        }
+    }
+}
