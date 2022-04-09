e5be7b4e6b7657d6f150f849f0303717c8537f12
felipe.pascoal <bd.bumdigital@gmail.com>
2022-04-09 01:47:02 -0300

    Exercicio 073 em andamento

--- Exercicio072/Program.cs
+++ Exercicio072/Program.cs
@@ -2,11 +2,46 @@
 
 namespace Exercicio072
 {
+    //72) Crie um programa que tenha uma dupla totalmente
+    //preenchida com uma contagem por extenso, de zero até
+    //vinte. Seu programa deverá ler um número pelo teclado
+    //(entre 0 e 20) e mostrá-lo por extenso.
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] numeros = {"zero", "um", "dois", "três", "quatro",
+            "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze",
+            "quatorze", "quinze", "dezesseis", "dezessete", "dezoioto", "dezenove",
+            "vinte"};
+            string continuar = "S";
+            while (continuar == "S")
+            {
+                Console.Write("Digite um numero: ");
+                int numero = int.Parse(Console.ReadLine());
+                while (numero < 0 || numero > 20)
+                {
+                    Console.Write("Numero fora do intervalo de 0 a 20. Digite novamente: ");
+                    numero = int.Parse(Console.ReadLine());
+                }           
+                for (int i = 0; i <= 20; i++)
+                {
+                    string numerosIndex = numeros[i];
+                    int index = Array.IndexOf(numeros, numerosIndex);
+                    if (numero == index)
+                    {
+                        Console.WriteLine($"Você digitou o numero {numeros[i].ToUpper()}");
+                    }
+                }
+                Console.Write("Quer continuar? [S/N]: ");
+                continuar = Console.ReadLine().ToUpper();
+                while (continuar != "S" && continuar != "N")
+                {
+                    Console.Write("Resposta invalida. Para continuar digite S " +
+                        "e para encerrar digite N [S/N]: ");
+                    continuar = Console.ReadLine().ToUpper();
+                }
+            }
         }
     }
 }
---
+++ Exercicio073/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio073
+{
+    //73) Crie uma tupla preenchida com os 20 primeiros 
+    //    colocados da Tabela do Campeonato Brasileiro de 
+    //    Futebol, na ordem de colocação.Depois mostre:
+    //a) Os 5 primeiros times.
+    //b) Os últimos 4 colocados.
+    //c) Times em ordem alfabética.
+    //d) Em que posição está o time da Chapecoense.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] times = {"Flamengo", "Palmeiras", "Atlético Mineiro", "Grêmio",
+                "Athletico Paranaense", "Santos", "São Paulo", "Internacional",
+                "Fluminense", "Corinthians", "Fortaleza", "Bahia", "Ceará", "Cruzeiro",
+                "América Mineiro", "Atlético Goianiense", "Chapecoense", "Botafogo",
+                "Vasco da Gama", "Red Bull Bragantino"};
+            Console.WriteLine("Os 5 primeiros colocados são:");
+            Console.WriteLine("################################");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{times[i]}");
+            }
+            Console.WriteLine("################################");
+            Console.WriteLine("Os 4 últimos colocados são:");
+            Console.WriteLine("################################");
+            for (int i = times.Length -4 ; i < times.Length; i++)
+            {
+                Console.WriteLine($"{times[i]}");
+            }
+            Console.WriteLine("################################");
+            Console.WriteLine($"O Chapecoence está na {Array.IndexOf(times, "Chapecoense")+1}° posição");
+        }
+    }
+}
