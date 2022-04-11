4627b1da67ded11270d7e710017e385a77620337
felipe.pascoal <bd.bumdigital@gmail.com>
2022-04-10 23:44:00 -0300

    Exercicio 076

---
+++ Exercicio075/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exercicio075
+{
+    //75) Desenvolva um programa que leia quatro valores
+    //pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+    //A) Quantas vezes apareceu o valor 9.
+    //B) Em que posição foi digitado o primeiro valor 3.
+    //C) Quais foram os números pares.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[4];
+            int contador9 = 0;
+            int contadorPares = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Digite um numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == 9)
+                {
+                    contador9++;
+                }
+            }
+            if (contador9 > 0)
+            {
+                Console.WriteLine($"A quantidade de numeros 9 digitadoe é {contador9}");
+            } else
+            {
+                Console.WriteLine("O numero 9 não foi digitado");
+            }
+            int valor3 = Array.IndexOf(numeros, 3);
+            if (valor3 != -1)
+            {
+                Console.WriteLine($"O numero 3 está na posição {valor3+1} do vetor");
+            } else
+            {
+                Console.WriteLine("O numero 3 não foi digitado");
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] % 2 == 0 && contadorPares == 0)
+                {
+                    contadorPares++;
+                    Console.Write($"Os numeros pares são {numeros[i]}");
+                } else if (numeros[i] % 2 == 0 && contadorPares != 0)
+                {
+                    Console.Write($", {numeros[i]}");
+                }
+            }
+            if (contadorPares == 0)
+            {
+                Console.WriteLine("Não foram digitados numeros pares");
+            }
+        }
+    }
+}
---
+++ Exercicio076/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio076
+{
+    //76) Crie um programa que tenha uma tupla única com
+    //nomes de produtos e seus respectivos preços, na
+    //sequência. No final, mostre uma listagem de preços,
+    //organizando os dados em forma tabular.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] produtos = {"macarrão", "11,50", "maionese", "8,90", 
+                "fandangos", "8,50"};
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                Console.Write($"{}");
+            }
+        }
+    }
+}
