31cf7ffe6036a28ea1c672652e8f69b7322b168c
felipe.pascoal <bd.bumdigital@gmail.com>
2022-04-17 00:00:39 -0300

    Exercicio 083 em andamento

--- Exercicio081/Program.cs
+++ Exercicio081/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercicio081
 {
@@ -12,7 +14,47 @@ namespace Exercicio081
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numeros = new List<int>();
+            string continuar = "S";
+            int digitados = 0;
+            int digi5 = 0;
+            while (continuar != "N")
+            {
+                Console.Write("Digite um numero: ");
+                numeros.Add(int.Parse(Console.ReadLine()));
+                digitados++;
+                if (numeros[digitados-1] == 5)
+                {
+                    digi5++; 
+                }
+                Console.Write("Quer continuar? [S/N]: ");
+                continuar = Console.ReadLine().ToUpper();
+                while (continuar != "S" && continuar != "N")
+                {
+                    Console.Write("Valor invalido! Para continuar digite S e para " +
+                        "encerrar digite N. [S/N]: ");
+                    continuar = Console.ReadLine().ToUpper();
+                }
+            }
+            Console.WriteLine("#########################################");
+            Console.WriteLine($"Foram digitados {digitados} numeros");
+            List<int> ordenado = numeros.OrderBy(x => x).ToList();
+            List<int> invertido = Enumerable.Reverse(ordenado).ToList();
+            Console.Write("A ordem decrescente dos numeros digitados é ");
+            for (int i = 0; i < invertido.Count; i++) 
+            {
+                string ifen = i != invertido.Count - 1? " - " : "";
+                Console.Write($"{invertido[i]}{ifen}");
+            }
+            Console.WriteLine();
+            if (digi5 == 0)
+            {
+                Console.WriteLine("O numero 5 não foi digitado");
+            } else
+            {
+                Console.WriteLine($"O numero 5 foi digitado {digi5} vezes");
+            }
+            Console.WriteLine("#########################################");
         }
     }
 }
---
+++ Exercicio082/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio082
+{
+    //82) Crie um programa que vai ler vários números e
+    //colocar em uma lista. Depois disso, crie duas listas
+    //extras que vão conter apenas os valores pares e os
+    //valores ímpares digitados, respectivamente. Ao final,
+    //mostre o conteúdo das três listas geradas.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            List<int> par = new List<int>();
+            List<int> impar = new List<int>();
+            string continuar = "S";
+            int contador = 0;
+            while (continuar != "N")
+            {
+                Console.Write("Digite um numero: ");
+                numeros.Add(int.Parse(Console.ReadLine()));
+                contador++;
+                if (numeros[contador-1] % 2 == 0)
+                {
+                    par.Add(numeros[contador-1]);
+                } else
+                {
+                    impar.Add(numeros[contador-1]);
+                }
+                Console.Write("Quer continuar? [S/N]: ");
+                continuar = Console.ReadLine().ToUpper();
+                while (continuar != "N" && continuar != "S")
+                {
+                    Console.Write("Valor invalido! Para continuar digite S e para " +
+                        "encerrar digite N. [S/N]: ");
+                    continuar = Console.ReadLine().ToUpper();
+                }
+            }
+            Console.WriteLine("####################################################");
+            Console.Write("Numeros digitos: ");
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                string ifen = i == numeros.Count - 1 ? "" : " - ";
+                Console.Write($"{numeros[i]}{ifen}");
+            }
+            Console.WriteLine();
+            Console.Write("Valores par: ");
+            for (int i = 0; i < par.Count; i++)
+            {
+                string ifen = i == par.Count - 1 ? "" : " - ";
+                Console.Write($"{par[i]}{ifen}");
+            }
+            Console.WriteLine();
+            Console.Write("Valores impar: ");
+            for (int i = 0; i < impar.Count; i++)
+            {
+                string ifen = i == impar.Count -1? "" : " - ";
+                Console.Write($"{impar[i]}{ifen}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("####################################################");
+        }
+    }
+}
